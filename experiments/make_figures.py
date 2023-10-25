c7c3ccc43b375ba994d3938fbf358ac92717629a
motiwari <mohittiwarinyc+github@gmail.com>
2023-10-24 18:28:34 -0700

    Refactoring create_configs to allow reuse in make_figures

--- experiments/make_figures.py
+++ experiments/make_figures.py
@@ -1,55 +1,22 @@
 import numpy as np
-from typing import List
+import pandas as pd
 
 from constants import (
     DATASETS_AND_LOSSES,
     ALL_ALGORITHMS,
 )
 
-def str_to_bool(s: str) -> bool:
-    if s == 'True':
-        return True
-    elif s == 'False':
-        return False
-    else:
-        raise ValueError(f"String {s} cannot be converted to bool")
-
-    
-def get_exp_name(exp: dict) -> str:
-    return str(
-        exp['dataset'] + '_' +
-        exp['loss'] + '_' +
-        exp['algorithm'] + '_' +
-        str(exp['n']) + '_' +
-        str(exp['k']) + '_' +
-        str(exp['T']) + '_' +
-        str(exp['cache_width']) + '_' +
-        str(exp['build_confidence']) + '_' +
-        str(exp['swap_confidence']) + '_' +
-        str(exp['parallelize']) + '_' +
-        str(exp['seed'])
-    )
+def generate_config():
+    """
+    Generates the config file for all experiments from the paper.
+    """
+    added_exps = []
 
 
-def get_exp_params_from_name(exp_name: str) -> dict:
-    tokens = exp_name.split('_')
-    return {
-        'dataset': tokens[0],
-        'loss': tokens[1],
-        'algorithm': tokens[2],
-        'n': int(tokens[3]),
-        'k': int(tokens[4]),
-        'T': int(tokens[5]),
-        'cache_width': int(tokens[6]),
-        'build_confidence': int(tokens[7]),
-        'swap_confidence': int(tokens[8]),
-        'parallelize': str_to_bool(tokens[9]),
-        'seed': int(tokens[10]),
-    }
 
 
-def get_table_1_exps() -> List:
-    exps = []
+
+def make_table_1():
     for n in [10000, 15000, 20000, 25000, 30000]:
         for dataset, loss in DATASETS_AND_LOSSES:
             if dataset in ["MNIST", "CIFAR10"]:
@@ -79,12 +46,12 @@ def get_table_1_exps() -> List:
                         'seed': seed,
                     }
                     exp_name = get_exp_name(exp)
-                    exps.append(exp_name)
-    return exps
+                    if exp_name not in added_exps:
+                        added_exps.append(exp_name)
 
+def make_figures_1_and_2():
+    # For Fig 1 and Fig 2
 
-def get_figures_1_and_2_exps() -> List:
-    exps = []
     for dataset, loss in DATASETS_AND_LOSSES:
         if dataset in ["MNIST", "CIFAR10"]:
             k = 10
@@ -125,12 +92,13 @@ def get_figures_1_and_2_exps() -> List:
                         'seed': seed,
                     }
                     exp_name = get_exp_name(exp)
-                    exps.append(exp_name)
-    return exps
+                    if exp_name not in added_exps:
+                        added_exps.append(exp_name)
 
 
-def get_figure_3_exps() -> List:
-    exps = []
+
+
+def make_figure_3():
     for dataset, loss in DATASETS_AND_LOSSES:
         k_schedule = [5, 10, 15]
 
@@ -164,12 +132,12 @@ def get_figure_3_exps() -> List:
                         'seed': seed,
                     }
                     exp_name = get_exp_name(exp)
-                    exps.append(exp_name)
-    return exps
+                    if exp_name not in added_exps:
+                        added_exps.append(exp_name)
 
+def make_appendix_table_1():
+    # For Appendix Table 1
 
-def get_appendix_table_1_exps() -> List:
-    exps = []
     for dataset, loss in DATASETS_AND_LOSSES:
         n = 10000
 
@@ -204,12 +172,13 @@ def get_appendix_table_1_exps() -> List:
                         'seed': seed,
                     }
                     exp_name = get_exp_name(exp)
-                    exps.append(exp_name)
-    return exps
+                    if exp_name not in added_exps:
+                        added_exps.append(exp_name)
 
 
-def get_appendix_figure_1_exps() -> List:
-    exps = []
+def make_appendix_figure_1():
+    # For Appendix Figure 1
+
     for dataset, loss in DATASETS_AND_LOSSES:
         n = 10000
 
@@ -243,12 +212,14 @@ def get_appendix_figure_1_exps() -> List:
                         'seed': seed,
                     }
                     exp_name = get_exp_name(exp)
-                    exps.append(exp_name)
-    return exps
+                    if exp_name not in added_exps:
+                        added_exps.append(exp_name)
 
 
-def get_appendix_table_2_exps() -> List:
-    exps = []
+
+def make_appendix_table_2():
+    # For Appendix Table 2
+
     for dataset, loss in DATASETS_AND_LOSSES:
         n = 10000
         k_schedule = [5, 10, 15]
@@ -275,53 +246,17 @@ def get_appendix_table_2_exps() -> List:
                         'seed': seed,
                     }
                     exp_name = get_exp_name(exp)
-                    exps.append(exp_name)
-    return exps
+                    if exp_name not in added_exps:
+                        added_exps.append(exp_name)
 
+def make_all_figures():
+    make_table_1()
+    make_figures_1_and_2()
+    make_figure_3()
+    make_appendix_table_1()
+    make_appendix_figure_1()
+    make_appendix_table_2()
 
-def add_exps(exps: List, new_exps: List):
-    for exp in new_exps:
-        if exp not in exps:
-            exps.append(exp)
-
-
-def generate_config():
-    """
-    Generates the config file for all experiments from the paper.
-    """
-    added_exps = []
-
-    # For Table 1
-    table_1_exps = get_table_1_exps()
-    add_exps(added_exps, table_1_exps)
-
-    # For Figures 1 and 2
-    figures_1_and_2_exps = get_figures_1_and_2_exps()
-    add_exps(added_exps, figures_1_and_2_exps)
-
-    # For Figure 3
-    figure_3_exps = get_figure_3_exps()
-    add_exps(added_exps, figure_3_exps)
-
-    # For Appendix Table 1
-    appendix_table_1_exps = get_appendix_table_1_exps()
-    add_exps(added_exps, appendix_table_1_exps)
-
-    # For Appendix Figure 1
-    appendix_figure_1_exps = get_appendix_figure_1_exps()
-    add_exps(added_exps, appendix_figure_1_exps)
-
-    # For Appendix Table 2
-    appendix_table_2_exps = get_appendix_table_2_exps()
-    add_exps(added_exps, appendix_table_2_exps)
-
-    # Write all configs to file
-    with open("all_configs.csv", "w+") as fout:
-        for exp_name in added_exps:
-            fout.write(exp_name + '\n')
 
 if __name__ == "__main__":
-    generate_config()
-
-
-
+    make_all_figures()
