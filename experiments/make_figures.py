81d67920fc7ade437ea06ad3d093379475ec7e37
motiwari <mohittiwarinyc+github@gmail.com>
2023-10-25 16:28:51 -0700

    Appendix Figure 1 made

--- experiments/make_figures.py
+++ experiments/make_figures.py
@@ -123,8 +123,8 @@ def make_figures_1_and_2():
         for algorithm_idx, algorithm in enumerate(ALL_ALGORITHMS):
             algo_results = fig1_2_results[(fig1_2_results['algorithm'] == algorithm) & (fig1_2_results['dataset'] == dataset) & (fig1_2_results['loss'] == loss)]
             xs = pd.to_numeric(algo_results['n'])
-            times = pd.to_numeric(algo_results['Total time']) / (1000*(pd.to_numeric(algo_results['T']) + 1))
-            samples = pd.to_numeric(algo_results['Total distance comps']) / (pd.to_numeric(algo_results['T']) + 1)
+            times = pd.to_numeric(algo_results['Total time']) / (1000*(pd.to_numeric(algo_results['Number of Steps']) + 1))
+            samples = pd.to_numeric(algo_results['Total distance comps']) / (pd.to_numeric(algo_results['Number of Steps']) + 1)
 
             ax1[dataset_idx // 2, dataset_idx % 2].plot(xs, times, label=algorithm, marker='o')
             ax2[dataset_idx // 2, dataset_idx % 2].plot(xs, samples, label=algorithm, marker='o')
@@ -170,11 +170,13 @@ def make_figures_1_and_2():
     fig2.tight_layout()
     fig1.set_size_inches(16, 10)
     fig2.set_size_inches(16, 10)
+
     handles1, labels1 = ax1[0, 0].get_legend_handles_labels()
     fig1.legend(handles1, labels1, loc='center', ncol=4)
 
     handles2, labels2 = ax2[0, 0].get_legend_handles_labels()
     fig2.legend(handles2, labels2, loc='center', ncol=4)
+
     fig1.savefig(os.path.join("figures", "figure1.pdf"), format='pdf')
     fig2.savefig(os.path.join("figures", "figure2.pdf"), format='pdf')
 
@@ -193,8 +195,8 @@ def make_figure_3():
                 (fig3_results['algorithm'] == algorithm) & (fig3_results['dataset'] == dataset) & (
                             fig3_results['loss'] == loss)]
             xs = pd.to_numeric(algo_results['k'])
-            times = pd.to_numeric(algo_results['Total time']) / (1000 * (pd.to_numeric(algo_results['T']) + 1))
-            samples = pd.to_numeric(algo_results['Total distance comps']) / (pd.to_numeric(algo_results['T']) + 1)
+            times = pd.to_numeric(algo_results['Total time']) / (1000 * (pd.to_numeric(algo_results['Number of Steps']) + 1))
+            samples = pd.to_numeric(algo_results['Total distance comps']) / (pd.to_numeric(algo_results['Number of Steps']) + 1)
 
             ax3[dataset_idx // 2, dataset_idx % 2].plot(xs, times, label=algorithm, marker='o')
             ax4[dataset_idx // 2, dataset_idx % 2].plot(xs, samples, label=algorithm, marker='o')
@@ -242,11 +244,13 @@ def make_figure_3():
     fig4.tight_layout()
     fig3.set_size_inches(16, 10)
     fig4.set_size_inches(16, 10)
+
     handles1, labels1 = ax3[0, 0].get_legend_handles_labels()
     fig3.legend(handles1, labels1, loc='center', ncol=4)
 
     handles2, labels2 = ax4[0, 0].get_legend_handles_labels()
     fig4.legend(handles2, labels2, loc='center', ncol=4)
+
     fig3.savefig(os.path.join("figures", "figure3.pdf"), format='pdf')
     fig4.savefig(os.path.join("figures", "figure4.pdf"), format='pdf')
 
@@ -255,7 +259,58 @@ def make_appendix_table_1():
 
 
 def make_appendix_figure_1():
-    pass
+    afig1_exps = get_figure_3_exps()
+    afig1_results = get_pd_from_exps(afig1_exps)
+
+    afig1, axa1 = plt.subplots(2, 2)
+
+    for dataset_idx in range(4):
+        dataset, loss = DATASETS_AND_LOSSES[dataset_idx]
+        algorithm = "BP++"
+        algo_results = afig1_results[
+            (afig1_results['algorithm'] == algorithm) & (afig1_results['dataset'] == dataset) & (
+                    afig1_results['loss'] == loss)]
+        xs = (pd.to_numeric(algo_results['Number of Steps'])).astype(int)
+        losses = pd.to_numeric(algo_results['Final loss'])
+        axa1[dataset_idx // 2, dataset_idx % 2].plot(xs, losses, label=algorithm, marker='o')
+
+        # Set title
+        dataset_to_title = {
+            "MNIST": "MNIST",
+            "CIFAR10": "CIFAR10",
+            "SCRNA": "scRNA",
+            "NEWSGROUPS": "20 Newsgroups",
+        }
+
+        loss_to_title = {
+            "L1": "$L_1$",
+            "L2": "$L_2$",
+            "cos": "Cosine",
+        }
+
+        ks = {
+            "MNIST": 10,
+            "CIFAR10": 10,
+            "SCRNA": 5,
+            "NEWSGROUPS": 5,
+        }
+
+        axa1[dataset_idx // 2, dataset_idx % 2].ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+        axa1[dataset_idx // 2, dataset_idx % 2].ticklabel_format(axis='x', style='plain')
+
+        axa1[dataset_idx // 2, dataset_idx % 2].set_title(
+            f"{dataset_to_title[dataset]}, {loss_to_title[loss]}, $n = 10000$, $k = {ks[dataset]}$")
+        axa1[dataset_idx // 2, dataset_idx % 2].grid()
+
+        # if dataset_idx % 2 == 0:  # Apply y label to right column
+        axa1[dataset_idx // 2, dataset_idx % 2].set_ylabel("Final clustering loss")
+
+        # if dataset_idx // 2 == 1:  # Apply x label to bottom row
+        axa1[dataset_idx // 2, dataset_idx % 2].set_xlabel("Number of SWAP iterations ($T$)")
+
+    afig1.tight_layout()
+    afig1.set_size_inches(16, 10)
+    afig1.savefig(os.path.join("figures", "appendix_figure1.pdf"), format='pdf')
 
 
 def make_appendix_table_2():
